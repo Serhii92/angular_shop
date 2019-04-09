993a944ef68769443d21f698500c2ab241e8d1ac
DESKTOP-NBA1BI2\Admin <nizhnichenko92@gmail.com>
2019-04-09 23:44:00 +0300

    Added CartItemStyleDirective

--- src/app/product/components/cart/cart.module.ts
+++ src/app/product/components/cart/cart.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CartListComponent } from './cart-list/cart-list.component';
 import { CartItemComponent } from './cart-item/cart-item.component';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../../../shared/shared.module';
 
 @NgModule({
   declarations: [CartListComponent, CartItemComponent],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
   exports: [CartListComponent, CartItemComponent]
 })
---
+++ src/app/shared/cart-item-style.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CartItemStyleDirective } from './cart-item-style.directive';
+
+describe('CartItemStyleDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CartItemStyleDirective();
+    expect(directive).toBeTruthy();
+  });
+});
